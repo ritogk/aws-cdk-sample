288f15c965b20b35c02e5ce108fa2d57ceda2772
ryoji <ryoji@unko.com>
2023-10-29 09:19:25 +0900

    一次コミット

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# aws-cdk-sample
+# AWS-CDK-SAMPLE
+## cdk-setup
+```
+cdk init app --language typescript
+```
+
+## ディレク構成
+bin: エントリーポイント  
+lib: インフラのコアコード  
+
+## tsからjsにビルド
+```
+npm run build
+```
+
+## AWSリソースの集合を表示する
+```
+cdk ls
+```
+
+## CloudFormationテンプレートを生成する
+```
+cdk synth
+```
+
+## CDKのデプロイに使うストレージを作成する
+```
+cdk bootstrap
+```
+
+## すべてのスタックをデプロイ
+```
+cdk deploy --all
+```
---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from "aws-cdk-lib"
+import { aws_s3 as s3 } from "aws-cdk-lib"
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    // new s3.Bucket(this, "MyFirstBucket", {
+    //   versioned: true,
+    // })
+    new s3.Bucket(this, "MyFirstBucket", {
+      // bucketName: "my-first-bucket", // 指定すると怒られる。
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // CDKスタックが削除されたときにバケットを削除する（オプション）
+    })
+  }
+}
